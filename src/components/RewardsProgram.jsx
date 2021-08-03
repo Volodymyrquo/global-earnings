f04b2b5a4183eeffabf8ec4c2b0ddda80b83721b
Volodymyr Basok <volodymyrquo@gmail.com>
2021-08-03 15:39:50 +0300

    added test expression

--- src/components/RewardsProgram.jsx
+++ src/components/RewardsProgram.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from 'react'
 const RewardsProgram = () => {
-    const rewardsStyle = {
-        color: "blue", 
-        fontWeight: "bold"
-    }
-    
-    return (
-        <div className='global-earnings-main' style={{ textAlign: "center", marginTop: "25%" }}>
-            <h1 style={rewardsStyle}> Hello from Rewards Program </h1>
-            <h2 style={rewardsStyle}> We are waiting for design </h2>
+  const rewardsStyle = {
+    color: 'blue',
+    fontWeight: 'bold',
+  }
 
-        </div>
-    );
-};
+  return (
+    <div
+      className='global-earnings-main'
+      style={{ textAlign: 'center', marginTop: '25%' }}>
+      <h1 style={rewardsStyle}>
+        {' '}
+        Hello from Rewards Program in the npm package{' '}
+      </h1>
+      <h2 style={rewardsStyle}> We are waiting for design </h2>
+    </div>
+  )
+}
 
-export default RewardsProgram;
+export default RewardsProgram
