2118b445a4521d0fddf97420e59d9a3d128062fb
Volodymyr Basok <volodymyrquo@gmail.com>
2021-08-06 10:19:23 +0300

    added common library from github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import 'common-library/src/assets/scss/styles.scss'
 import { GlobalEarningsBtn } from './components'
 import { Switch, Route, Redirect } from 'react-router-dom'
